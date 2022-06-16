3fc94b128ceb358a16dd60f316a33162e8bff874
jmindevelper <jmin.developer@gmail.com>
2022-06-16 15:53:34 +0900

    [rb] todayView 초기상태로 롤백

---
+++ AppStoreClone/AppStoreClone/Model/TodayCardModel.swift
@@ -0,0 +1,12 @@
+//
+//  TodayCardModel.swift
+//  AppStoreClone
+//
+//  Created by J_Min on 2022/06/15.
+//
+
+import Foundation
+
+struct TodayCardModel {
+    let image: String
+}
---
+++ AppStoreClone/AppStoreClone/ViewModel/TodayViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TodayViewModel.swift
+//  AppStoreClone
+//
+//  Created by J_Min on 2022/06/15.
+//
+
+import Foundation
+
+class TodayViewModel {
+    var cardItems: [TodayCardModel] = [
+        TodayCardModel(image: "1.jpg"),
+        TodayCardModel(image: "2.jpg"),
+        TodayCardModel(image: "3.jpg")
+    ]
+}
